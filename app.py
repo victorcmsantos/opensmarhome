48867773d4b5f8c361c68f6a56210f721d2c12c8
victor santos <victorcarlos.santos@gmail.com>
2020-12-22 18:49:58 +0100

    before sql

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
 from pathlib import Path
 import os
+
 
 app = Flask(__name__)
 
